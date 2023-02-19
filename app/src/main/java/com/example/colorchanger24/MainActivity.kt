1a640f666385719d5c712ffb67ace55ca9cd0b06
Nathanael Kim <nateykim@gmail.com>
2023-02-18 20:18:11 -0800

    Switch functionality

--- app/src/main/java/com/example/colorchanger24/MainActivity.kt
+++ app/src/main/java/com/example/colorchanger24/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
 
     var tvColor: TextView? = null
     var mycolor = intArrayOf(255, 127, 0)
+    var savedColor = intArrayOf(0, 0, 0, 0, 0) // extra long for indexing
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,14 +43,12 @@ class MainActivity : AppCompatActivity() {
             mycolor[0] = 255
             mycolor[1] = 255
             mycolor[2] = 255
-
             for (i in 0..2) {
                 var myID = "sb_$i"
                 var resID = resources.getIdentifier(myID, "id", packageName)
                 val mySeekBar = findViewById(resID) as SeekBar
                 mySeekBar.setProgress(255, true)
             }
-
             tvColor?.setBackgroundColor(
                 Color.argb(
                     255,
@@ -56,30 +56,15 @@ class MainActivity : AppCompatActivity() {
                 )
             )
         }
+
 
         val switch0 = findViewById<Switch>(R.id.switch0);
-        switch0.setOnClickListener{
-            mycolor[0] = 0
-            tvColor?.setBackgroundColor(
-                Color.argb(
-                    255,
-                    mycolor[0], mycolor[1], mycolor[2]
-                )
-            )
-        }
-        val switch1 = findViewById<Switch>(R.id.switch1);
-        switch1.setOnClickListener{
-            mycolor[1] = 0
-            tvColor?.setBackgroundColor(
-                Color.argb(
-                    255,
-                    mycolor[0], mycolor[1], mycolor[2]
-                )
-            )
-        }
-        val switch2 = findViewById<Switch>(R.id.switch2);
-        switch2.setOnClickListener{
-            mycolor[2] = 0
+        switch0?.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                mycolor[0] = savedColor[0]
+            }else{
+                mycolor[0] = 0
+            }
             tvColor?.setBackgroundColor(
                 Color.argb(
                     255,
@@ -88,19 +73,49 @@ class MainActivity : AppCompatActivity() {
             )
         }
 
+        val switch1 = findViewById<Switch>(R.id.switch1);
+        switch1?.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                mycolor[1] = savedColor[1]
+            }else{
+                mycolor[1] = 0
+            }
+            tvColor?.setBackgroundColor(
+                Color.argb(
+                    255,
+                    mycolor[0], mycolor[1], mycolor[2]
+                )
+            )
+        }
+
+        val switch2 = findViewById<Switch>(R.id.switch2);
+        switch2?.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                mycolor[2] = savedColor[2]
+            }else{
+                mycolor[2] = 0
+            }
+            tvColor?.setBackgroundColor(
+                Color.argb(
+                    255,
+                    mycolor[0], mycolor[1], mycolor[2]
+                )
+            )
+        }
     }
 
     var myListener: SeekBar.OnSeekBarChangeListener = object : SeekBar.OnSeekBarChangeListener {
         override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
             val myLayout = seekBar.parent as ConstraintLayout
-            val myIndex = myLayout.indexOfChild(seekBar)
-            var myTextView = myLayout.getChildAt(myIndex + 1) as TextView
+            val myIndex = myLayout.indexOfChild(seekBar)                            // Which seekbar is being selected
 
-            var preDouble = i * 100 / 255
-            var double = preDouble.toDouble().roundToInt() / 100.0
-            myTextView.text = ("" + double)
+            var myTextView = myLayout.getChildAt(myIndex + 1) as TextView
+            savedColor[myIndex - 1] = i                                             // saves the color for toggle switch
+            var preDouble = i * 100 / 255                                           // converts seekbar position (out of 255) into a number between 0 and 1
+            var double = preDouble.toDouble().roundToInt() / 100.0                  // Rounds to the nearest 100th position
+            myTextView.text = ("" + double)                                         // Displays number
             mycolor[seekBar.tag.toString().toInt()]= i
-            tvColor?.setBackgroundColor(
+            tvColor?.setBackgroundColor(                                            // Set color
                 Color.argb(
                     255,
                     mycolor[0], mycolor[1], mycolor[2]
